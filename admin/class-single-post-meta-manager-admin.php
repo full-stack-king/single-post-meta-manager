d67a1593d949088f0afc95f69c84b1503c1f58be
rajakannan <rajacse10@gmail.com>
2014-05-14 18:15:36 +0530

    inital files and codes

---
+++ admin/class-single-post-meta-manager-admin.php
@@ -0,0 +1,18 @@
+<?php 
+
+class Single_Post_Meta_Manager_Admin{
+	
+	protected $version;
+
+	public function __construct( $version ){
+		$this->version = $version;
+	}
+
+	public function enqueue_styles(){
+	}
+
+	public function add_meta_box(){
+	}
+
+}
+?>
---
+++ single-post-meta-manager.php
@@ -0,0 +1,35 @@
+<?php
+/*
+Plugin Name: Single Post Meta Manager
+Description: Single Post Meta Manager displays the post meta data associated with a given post.
+Plugin URI: http://github.com/tommcfarlin/post-meta-manager
+Author: Raja Kannan
+Author URI: http://rajakannan.com
+Version: 0.1.0
+License: GPL2
+License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+Text Domain: single-post-meta-manager-locale
+Domain Path: /languages
+*/
+
+/*
+
+    Copyright (C) 2014  Raja Kannan rajacse10@gmail.com
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
